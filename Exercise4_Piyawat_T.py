9f9864b44df614a0fda7e8f00f2602322c101732
Jggok <150037787+Jggok@users.noreply.github.com>
2024-02-05 20:41:19 +0700

    Update Exercise4_Piyawat_T.py

--- Exercise4_Piyawat_T.py
+++ Exercise4_Piyawat_T.py
@@ -1,1 +1,10 @@
+FE = 85
+GB = 70
+ICS = 88
+CP = 90
 
+print ("--- Your Score ---")
+print ("Foundation English :",FE)
+print ("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
